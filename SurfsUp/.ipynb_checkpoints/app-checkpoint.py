f4aaa800be984c1d6e560bdefb4d56208bd44077
PapaKLK <kjkrause1323@gmail.com>
2023-06-06 09:11:55 -0500

    Initial Load

---
+++ SurfsUp/.ipynb_checkpoints/app-checkpoint.py
@@ -0,0 +1,43 @@
+{
+ "cells": [
+  {
+   "cell_type": "markdown",
+   "id": "da036ea5",
+   "metadata": {},
+   "source": [
+    "### Kevin Krause\n",
+    "### Module 10 Challenge\n",
+    "### June 12, 2023"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "1b9f2941",
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "Python 3 (ipykernel)",
+   "language": "python",
+   "name": "python3"
+  },
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.10.10"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 5
+}
